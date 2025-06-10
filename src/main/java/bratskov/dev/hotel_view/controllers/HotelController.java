23595bb24f9dfc341c1b8156944d4c64298f21d2
timofei <tima.bratskov05@mail.ru>
2025-06-10 14:55:07 +0300

    add endpoint(post histogram)

--- src/main/java/bratskov/dev/hotel_view/controllers/HotelController.java
+++ src/main/java/bratskov/dev/hotel_view/controllers/HotelController.java
@@ -1,6 +1,7 @@
 package bratskov.dev.hotel_view.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import bratskov.dev.hotel_view.dtos.HotelFullDto;
 import bratskov.dev.hotel_view.dtos.HotelShortDto;
 import bratskov.dev.hotel_view.services.HotelService;
@@ -12,8 +13,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -22,11 +25,6 @@ import java.util.UUID;
 public class HotelController {
 
     private final HotelService hotelService;
-
-    @GetMapping("/hotels")
-    public List<HotelShortDto> getAllHotels() {
-        return hotelService.getAllHotels();
-    }
 
     @GetMapping("/search")
     public List<HotelShortDto> searchHotels(
@@ -41,19 +39,33 @@ public class HotelController {
         return hotelService.searchHotels(name, brand, city, country, amenity, sortBy, direction);
     }
 
-    @PostMapping("/hotels")
-    public void createHotel(@RequestBody CreateHotelRequest request) {
-        hotelService.createHotel(request);
-    }
-
     @GetMapping("/hotels/{id}")
     public HotelFullDto getHotel(@PathVariable UUID id) {
         return hotelService.getHotelById(id);
+    }
+
+    @GetMapping("/hotels")
+    public List<HotelShortDto> getAllHotels() {
+        return hotelService.getAllHotels();
+    }
+
+    @PostMapping("/hotels")
+    public void createHotel(@RequestBody CreateHotelRequest request) {
+        hotelService.createHotel(request);
     }
 
     @PostMapping("/hotels/{id}/amenities")
     public void addAmenities(@PathVariable UUID id,
                              @RequestBody List<String> amenities) {
         hotelService.addAmenities(id, amenities);
+    }
+
+    @GetMapping("/histogram/{param}")
+    public Map<String, Long> getHistogram(@PathVariable String param) {
+        if (!"brand".equals(param) && !"city".equals(param) && !"country".equals(param) && !"amenities".equals(param)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "Недопустимый параметр для гистограммы. Разрешены: brand, city, country, amenities.");
+        }
+        return hotelService.getHistogram(param);
     }
 }
--- src/main/java/bratskov/dev/hotel_view/services/HotelService.java
+++ src/main/java/bratskov/dev/hotel_view/services/HotelService.java
@@ -1,16 +1,24 @@
 package bratskov.dev.hotel_view.services;
 
+import jakarta.persistence.Tuple;
+import jakarta.persistence.TypedQuery;
 import lombok.RequiredArgsConstructor;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.Root;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import jakarta.persistence.criteria.Expression;
 import bratskov.dev.hotel_view.dtos.HotelFullDto;
+import jakarta.persistence.criteria.CriteriaQuery;
 import bratskov.dev.hotel_view.dtos.HotelShortDto;
 import bratskov.dev.hotel_view.mappers.HotelMapper;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import bratskov.dev.hotel_view.entities.HotelEntity;
 import bratskov.dev.hotel_view.dtos.CreateHotelRequest;
 import org.springframework.data.jpa.domain.Specification;
 import bratskov.dev.hotel_view.repositories.HotelRepository;
 
+import java.util.Map;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -20,17 +28,19 @@ import java.util.stream.Collectors;
 public class HotelService {
     private final HotelRepository hotelRepository;
     private final HotelMapper mapper;
+    private final EntityManager entityManager;
 
-    public List<HotelShortDto> getAllHotels(){
+    public List<HotelShortDto> getAllHotels() {
         List<HotelEntity> hotelEntities = hotelRepository.findAll();
         return hotelEntities.stream()
                 .map(mapper::toShortDto)
                 .collect(Collectors.toList());
     }
 
-    public List<HotelShortDto> searchHotels(String name, String brand, String city,
-                                            String country, String amenity,
-                                            String sortBy, String direction) {
+    public List<HotelShortDto> searchHotels(
+            String name, String brand, String city,
+            String country, String amenity,
+            String sortBy, String direction) {
 
         Specification<HotelEntity> spec = (root, query, cb) -> cb.conjunction();
 
@@ -69,9 +79,34 @@ public class HotelService {
         hotelRepository.save(hotelEntity);
     }
 
-    public void addAmenities(UUID id, List<String> amenities){
+    public void addAmenities(UUID id, List<String> amenities) {
         HotelEntity hotelEntity = hotelRepository.findById(id).orElseThrow();
         hotelEntity.getAmenities().addAll(amenities);
         hotelRepository.save(hotelEntity);
+    }
+
+    public Map<String, Long> getHistogram(String param) {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Tuple> cq = cb.createTupleQuery();
+        Root<HotelEntity> hotelEntityRoot = cq.from(HotelEntity.class);
+        Expression<?> groupByField;
+
+        if ("amenities".equals(param)) {
+            groupByField = hotelEntityRoot.join("amenities");
+        } else {
+            groupByField = hotelEntityRoot.get(param);
+            cq.where(cb.isNotNull(groupByField));
+        }
+        cq.multiselect(groupByField, cb.count(hotelEntityRoot));
+        cq.groupBy(groupByField);
+
+        TypedQuery<Tuple> query = entityManager.createQuery(cq);
+        List<Tuple> results = query.getResultList();
+
+        return results.stream()
+                .collect(Collectors.toMap(
+                        tuple -> tuple.get(0, String.class),
+                        tuple -> tuple.get(1, Long.class)
+                ));
     }
 }
