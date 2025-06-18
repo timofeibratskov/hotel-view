3639598418576e4334f3c04834f3f30ccc83b5d3
timofei <tima.bratskov05@mail.ru>
2025-06-18 18:14:09 +0300

    time check

--- src/main/java/bratskov/dev/hotel_view/entities/embeddeds/ArrivalTime.java
+++ src/main/java/bratskov/dev/hotel_view/entities/embeddeds/ArrivalTime.java
@@ -8,6 +8,7 @@ import lombok.ToString;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.NotBlank;
 import io.swagger.v3.oas.annotations.media.Schema;
 
@@ -22,7 +23,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 public class ArrivalTime {
     @NotBlank(message = "Check-in time must not be blank")
     @Schema(description = "Check-in time", example = "14:00")
+    @Pattern(regexp = "^([01]?[0-9]|2[0-3]):[0-5][0-9]$", message = "Check-in time must be in HH:MM format (e.g., 14:00)")
     String checkIn;
     @Schema(description = "Check-out time", example = "12:00")
+    @Pattern(regexp = "^([01]?[0-9]|2[0-3]):[0-5][0-9]$", message = "Check-in time must be in HH:MM format (e.g., 14:00)")
     String checkOut;
 }
